cffa405eb2a8230ed3b2d631f54fcc0d01a4b3f9
Ivan Tsiupa <vattgert@gmail.com>
2023-03-17 19:57:47 +0200

    change EOL to CRLF and some formatting changes in several files

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,12 @@
 import Fastify from "fastify";
 
 const fastify = Fastify({
-  logger: true
+  logger: true,
 });
 
-fastify.listen({ port: 3000}, function(error, address){
-  if(error){
+fastify.listen({ port: 3000 }, function (error, address) {
+  if (error) {
     fastify.log.error(error);
     process.exit(1);
   }
-})
+});
